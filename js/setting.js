29b0fa021387baf7674a38fc38bb5f29714be1f6
thanhdd <duythanh14189@gmail.com>
2025-06-24 23:04:59 +0700

    up

--- js/setting.js
+++ js/setting.js
@@ -18,26 +18,39 @@ $('.news-block__heading--all').on('click', function(){
     $(".news-block__fill li").removeClass("support");
     $(".news-block__fill li").removeClass("news");
     $(".news-block__fill li").addClass("all");
+    $(".news-block__heading li").removeClass("active");
+    $(this).addClass("active");
 });
 $('.news-block__heading--news').on('click', function(){
     $(".news-block__fill li").removeClass("all");
     $(".news-block__fill li").removeClass("event");
     $(".news-block__fill li").removeClass("support");
     $(".news-block__fill .news-block__fill--news").addClass("news");
+    $(".news-block__heading li").removeClass("active");
+    $(this).addClass("active");
 });
 $('.news-block__heading--event').on('click', function(){
     $(".news-block__fill li").removeClass("all");
     $(".news-block__fill li").removeClass("support");
     $(".news-block__fill li").removeClass("news");
     $(".news-block__fill .news-block__fill--event").addClass("event");
+    $(".news-block__heading li").removeClass("active");
+    $(this).addClass("active");
 });
 $('.news-block__heading--support').on('click', function(){
     $(".news-block__fill li").removeClass("all");
     $(".news-block__fill li").removeClass("news");
     $(".news-block__fill li").removeClass("event");
     $(".news-block__fill .news-block__fill--support").addClass("support");
+    $(".news-block__heading li").removeClass("active");
+    $(this).addClass("active");
 });
 
+$('.js-popup-ytb').on('click', function(e){
+    e.preventDefault();
+    $("#popup-youtube").addClass("active");
+    $("html").addClass("js-locked");
+});
 
 $(".procedure-show").slick({
   slidesToShow: 2,
